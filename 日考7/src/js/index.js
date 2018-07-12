fad6baec900df6d50897df1a287b958237c6db57
fcc <fcc980112@163.com>
2018-07-12 10:12:49 +0800

    上传完成

---
+++ 日考7/src/js/index.js
@@ -0,0 +1,6 @@
+define(['index'], function(index) {
+    var sum = function(a, b) {
+        a + b
+    }
+    return sum
+});
---
+++ 日考7/src/js/main.js
@@ -0,0 +1,7 @@
+require.config({
+    baseUrl: '../js',
+    paths: {
+        "jquery": "jquery-3.2.1.min",
+        "index": "index"
+    }
+})
